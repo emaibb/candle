8c8b9a0e743acead1d26299b96d2aeeffc98ec3f
emaibb <mzfengibb@outlook.com>
2024-11-19 19:19:04 +0800

    for cpu loading

--- candle-transformers/src/models/qwen2.rs
+++ candle-transformers/src/models/qwen2.rs
@@ -52,11 +52,11 @@ impl RotaryEmbedding {
             .map(|i| 1f32 / cfg.rope_theta.powf(i as f64 / dim as f64) as f32)
             .collect();
         let inv_freq_len = inv_freq.len();
-        let inv_freq = Tensor::from_vec(inv_freq, (1, inv_freq_len), dev)?.to_dtype(dtype)?;
+        let inv_freq = Tensor::from_vec(inv_freq, (1, inv_freq_len), dev)?;
         let t = Tensor::arange(0u32, max_seq_len as u32, dev)?
-            .to_dtype(dtype)?
-            .reshape((max_seq_len, 1))?;
-        let freqs = t.matmul(&inv_freq)?;
+            .reshape((max_seq_len, 1))?
+            .to_dtype(inv_freq.dtype())?;
+        let freqs = t.matmul(&inv_freq)?.to_dtype(dtype)?;
         Ok(Self {
             sin: freqs.sin()?,
             cos: freqs.cos()?,
